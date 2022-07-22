84ce2e92a0b88451ca80999c4ed7887cba27b385
sangjung.choi <sangjung.choi@hanmi.co.kr>
2022-07-22 14:22:53 +0900

    commit

--- pages/myVue7.vue
+++ pages/myVue7.vue
@@ -17,30 +17,11 @@
         </v-container>
       </template>
     </div>
-
-    <div class="example-modal-window">
-      <p>버튼을 누르면 모달 대화 상자가 열립니다.</p>
-      <button @click="openModal">열기</button>
-
-      <!-- 컴포넌트 MyModal -->
-      <MyModal @close="closeModal" v-if="modal">
-        <!-- default 슬롯 콘텐츠 -->
-        <p>Vue.js Modal Window!</p>
-        <div><input v-model="message" /></div>
-        <!-- /default -->
-        <!-- footer 슬롯 콘텐츠 -->
-        <template slot="footer">
-          <button @click="doSend">제출</button>
-        </template>
-        <!-- /footer -->
-      </MyModal>
-    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import myModal from './components/myModal.vue'
 export default {
   data() {
     return {
